328178b99b2938d499b1c5464b49519764918539
tianopo <matheuslink18@hotmail.com>
2024-01-03 09:14:55 -0300

    configuração de medidas

---
+++ settings/measures.tsx
@@ -0,0 +1,60 @@
+const base = {
+  /** # 0px */ xs: '0px',
+  /** # 1px */ px: '1px',
+  /** # 2px */ sm: '0.125rem',
+  /** # 4px */ md: '0.25rem',
+  /** # 6px */ lg: '0.375rem',
+  /** # 8px */ xl: '0.5rem',
+  /** # 10px */ xxl: '0.625rem',
+  /** # 12px */ '2xl': '0.75rem',
+  /** # 14px */ '3xl': '0.875rem',
+  /** # 16px */ '4xl': '1rem',
+  /** # 20px */ '5xl': '1.25rem',
+  /** # 24px */ '6xl': '1.5rem',
+  /** # 28px */ '7xl': '1.75rem',
+  /** # 32px */ '8xl': '2rem',
+  /** # 36px */ '9xl': '2.25rem',
+  /** # 40px */ '10xl': '2.5rem',
+  /** # 44px */ '11xl': '2.75rem',
+  /** # 48px */ '12xl': '3rem',
+  /** # 56px */ '14xl': '3.5rem',
+  /** # 64px */ '16xl': '4rem',
+  /** # 80px */ '20xl': '5rem',
+  /** # 96px */ '24xl': '6rem',
+  /** # 112px */ '28xl': '7rem',
+  /** # 128px */ '32xl': '8rem',
+  /** # 144px */ '36xl': '9rem',
+  /** # 160px */ '40xl': '10rem',
+  /** # 176px */ '44xl': '11rem',
+  /** # 192px */ '48xl': '12rem',
+  /** # 208px */ '52xl': '13rem',
+  /** # 224px */ '56xl': '14rem',
+  /** # 240px */ '60xl': '15rem',
+  /** # 256px */ '64xl': '16rem',
+  /** # 288px */ '72xl': '18rem',
+  /** # 320px */ '80xl': '20rem',
+  /** # 384px */ '96xl': '24rem',
+  /** # 100% */ full: '100%',
+}
+
+const borderRadius = {
+  ...base,
+}
+
+const gap = {
+  ...base,
+}
+
+const fontSize = {
+  ...base,
+}
+
+const padding = {
+  ...base,
+}
+
+const margin = {
+  ...base,
+}
+
+export { borderRadius, fontSize, gap, margin, padding }
