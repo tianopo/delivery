28f5d99f6e7f8b8045ec35ab7c8b48853d09264d
tianopo <matheuslink18@hotmail.com>
2024-01-03 09:52:46 -0300

    botao e input

---
+++ src/components/Buttons/Botao.tsx
@@ -0,0 +1,38 @@
+import { Button } from '@chakra-ui/react'
+import { ReactNode } from "react"
+import { corFundo, corPrimaria, corSecundaria } from "settings/colors"
+import { borderRadius, padding } from "settings/measures"
+
+export const Botao = ({ children, onClick }: IBotao) => {
+
+  return (
+    <Button
+      width={{ base: '100%', md: '85px' }}
+      height="46px"
+      cursor="pointer"
+      _active={{
+        transform: 'scale(0.98)',
+      }}
+      _focus={{}}
+      borderWidth={`1px`}
+      borderColor={corSecundaria}
+      borderRadius={borderRadius.lg}
+      display="flex"
+      fontWeight={'400'}
+      p={padding["2xl"]}
+      bg={corFundo.primaria}
+      _hover={{
+        opacity: 0.5,
+      }}
+      color={corPrimaria}
+      onClick={onClick}
+    >
+      {children}
+    </Button>
+  )
+}
+
+interface IBotao {
+  children: ReactNode
+  onClick: () => void
+}
